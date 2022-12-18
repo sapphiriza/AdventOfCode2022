fa42c12c98a2ff79ca0783ef946d3076b35ae816
sapphiriza <sapphiriza@gmail.com>
2022-12-18 13:17:55 -0800

    Day 3 part 2

--- Day3.py
+++ Day3.py
@@ -1,14 +1,31 @@
+def getprio(c):
+    if(c.isupper()):
+        return ord(c) - ord('A') + 27
+    else:
+        return ord(c) - ord('a') + 1
+
 if __name__ == "__main__":
-    file = open("input/rucksacks.txt")
+    lines = open("input/rucksacks.txt").readlines()
+
     prioritySum = 0
-    for line in file.readlines():
+    for line in lines:
         line = line.strip()
         middle = int(len(line) / 2)
         left = line[:middle]
         right = line[middle:]
         for c in left:
             if c in right:
-                priority = ord(c) - ord('A') + 27 if c.isupper() else ord(c) - ord('a') + 1
-                prioritySum += priority
+                prioritySum += getprio(c)
                 break
     print("The sum of priorities for all errored items is " + str(prioritySum))
+
+    badgeSum = 0
+    for i in range(0, len(lines), 3):
+        first = lines[i]
+        second = lines[i+1]
+        third = lines[i+2]
+        for c in first:
+            if c in second and c in third:
+                badgeSum += getprio(c)
+                break
+    print("The sum of priorities for all badges is " + str(badgeSum))
